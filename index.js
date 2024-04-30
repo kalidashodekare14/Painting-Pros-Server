2c4b92bd86009069c25efa2d984cd04a1ead94ea
Kalidash Odekare <shaktiadhikari49@gmail.com>
2024-04-30 17:13:54 +0600

    Painting pros server update

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.2rn0dld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
